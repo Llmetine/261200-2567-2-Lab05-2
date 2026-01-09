85164bd4835eb7c5f330b9e7d248b0fe68bd2bf7
Jon <ntjeddelog@gmail.com>
2026-01-09 16:02:57 +0700

    Update Lec05.java

--- Lec05.java
+++ Lec05.java
@@ -1,14 +1,16 @@
 void main() {
 
     // Create an instance of User named john
-    User john = new User("John", 1954, 2, 18);
+    User john = new User("John", 1954, 1, 9);
 
     // Display John's information
     john.displayInfo();
+    john.displayHappyBirthday();
     System.out.println();
 //    john.getNow();
-    Admin nicolas = new Admin("Nicolas", 1964, 1, 7);
+    Admin nicolas = new Admin("Nicolas", 1964, 1, 9);
     nicolas.displayInfo();
+    nicolas.displayHappyBirthday();
     System.out.println();
     nicolas.displayInfo(true);
     System.out.println();
